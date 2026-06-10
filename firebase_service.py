73cbb6de33eeb35b1c79bceb3bd66815350bb2ba
Lav Patel <163027320+PatelLav@users.noreply.github.com>
2026-06-10 21:41:34 +0530

    Update firebase_service.py

--- firebase_service.py
+++ firebase_service.py
@@ -1,17 +1,21 @@
 import json
 import os
 import firebase_admin
+from firebase_admin import credentials, messaging
 
-from firebase_admin import credentials
-from firebase_admin import messaging
+firebase_json = os.getenv("FIREBASE_CREDENTIALS")
 
-cred_dict = json.loads(
-    os.environ["FIREBASE_CREDENTIALS"]
-)
+print("Firebase env found:", firebase_json is not None)
+
+if not firebase_json:
+    raise Exception("FIREBASE_CREDENTIALS missing")
+
+cred_dict = json.loads(firebase_json)
 
 cred = credentials.Certificate(cred_dict)
 
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 
 async def firebase_send_push(token, title, body):
